caf2f69a80f790da69124d6471f6388479b23b93
SerjoA <bijobijo@gmail.com>
2025-02-08 20:25:37 +0200

    Update ActionContent.php

--- src/Actions/Concerns/ActionContent.php
+++ src/Actions/Concerns/ActionContent.php
@@ -17,276 +17,249 @@ use Rmsramos\Activitylog\Infolists\Components\TimeLineRepeatableEntry;
 use Rmsramos\Activitylog\Infolists\Components\TimeLineTitleEntry;
 use Spatie\Activitylog\Models\Activity;
 
-trait ActionContent
-{
-    private ?array $withRelations = null;
+trait ActionContent {
+   protected Closure $modifyQueryUsing;
+   protected Closure|Builder $query;
+   protected ?Closure $activitiesUsing;
+   protected ?Closure $modifyTitleUsing;
+   protected ?Closure $shouldModifyTitleUsing;
+   private ?array $withRelations = null;
+   private ?array $timelineIcons = null;
+   private ?array $timelineIconColors = null;
+   private ?int $limit = 10;
 
-    private ?array $timelineIcons = null;
+   public static function getDefaultName(): ?string {
+      return 'activitylog_timeline';
+   }
 
-    private ?array $timelineIconColors = null;
+   public function withRelations(?array $relations = null): ?StaticAction {
+      $this->withRelations = $relations;
 
-    private ?int $limit = 10;
+      return $this;
+   }
 
-    protected Closure $modifyQueryUsing;
+   public function timelineIcons(?array $timelineIcons = null): ?StaticAction {
+      $this->timelineIcons = $timelineIcons;
 
-    protected Closure|Builder $query;
+      return $this;
+   }
 
-    protected ?Closure $activitiesUsing;
+   public function timelineIconColors(?array $timelineIconColors = null): ?StaticAction {
+      $this->timelineIconColors = $timelineIconColors;
 
-    protected ?Closure $modifyTitleUsing;
+      return $this;
+   }
 
-    protected ?Closure $shouldModifyTitleUsing;
+   public function modifyQueryUsing(Closure $closure): static {
+      $this->modifyQueryUsing = $closure;
 
-    public static function getDefaultName(): ?string
-    {
-        return 'activitylog_timeline';
-    }
+      return $this;
+   }
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+   public function modifyTitleUsing(Closure $closure): static {
+      $this->modifyTitleUsing = $closure;
 
-        $this->configureInfolist();
-        $this->configureModal();
-        $this->activitiesUsing        = null;
-        $this->modifyTitleUsing       = null;
-        $this->shouldModifyTitleUsing = fn () => true;
-        $this->modifyQueryUsing       = fn ($builder) => $builder;
-        $this->modalHeading           = __('activitylog::action.modal.heading');
-        $this->modalDescription       = __('activitylog::action.modal.description');
-        $this->query                  = function (?Model $record) {
-            return Activity::query()
-                ->where(function (Builder $query) use ($record) {
-                    $query->where(function (Builder $q) use ($record) {
-                        $q->where('subject_type', $record->getMorphClass())
-                            ->where('subject_id', $record->getKey());
-                    })->when($this->getWithRelations(), function (Builder $query, array $relations) use ($record) {
-                        foreach ($relations as $relation) {
-                            $model = get_class($record->{$relation}()->getRelated());
-                            $query->orWhere(function (Builder $q) use ($record, $model, $relation) {
-                                $q->where('subject_type', (new $model)->getMorphClass())
-                                    ->whereIn('subject_id', $record->{$relation}()->pluck('id'));
-                            });
-                        }
-                    });
-                });
+      return $this;
+   }
 
-        };
-    }
+   public function shouldModifyTitleUsing(Closure $closure): static {
+      $this->shouldModifyTitleUsing = $closure;
 
-    private function configureInfolist(): void
-    {
-        $this->infolist(function (?Model $record, Infolist $infolist) {
-            return $infolist
-                ->state(['activities' => $this->getActivityLogRecord($record, $this->getWithRelations())])
-                ->schema($this->getSchema());
-        });
-    }
+      return $this;
+   }
 
-    private function configureModal(): void
-    {
-        $this->slideOver()
-            ->modalIcon('heroicon-o-eye')
-            ->modalFooterActions(fn () => [])
-            ->tooltip(__('activitylog::action.modal.tooltip'))
-            ->icon('heroicon-o-bell-alert');
-    }
+   public function activitiesUsing(Closure $closure): static {
+      $this->activitiesUsing = $closure;
 
-    private function getSchema(): array
-    {
-        return [
-            TimeLineRepeatableEntry::make('activities')
-                ->schema([
-                    TimeLineIconEntry::make('activityData.event')
-                        ->icon(function ($state) {
-                            return $this->getTimelineIcons()[$state] ?? 'heroicon-m-check';
-                        })
-                        ->color(function ($state) {
-                            return $this->getTimelineIconColors()[$state] ?? 'primary';
-                        }),
-                    TimeLineTitleEntry::make('activityData')
-                        ->configureTitleUsing($this->modifyTitleUsing)
-                        ->shouldConfigureTitleUsing($this->shouldModifyTitleUsing),
-                    TimeLinePropertiesEntry::make('activityData'),
-                    TextEntry::make('log_name')
-                        ->hiddenLabel()
-                        ->badge(),
-                    TextEntry::make('updated_at')
-                        ->hiddenLabel()
-                        ->since()
-                        ->badge(),
-                ]),
-        ];
-    }
+      return $this;
+   }
 
-    public function withRelations(?array $relations = null): ?StaticAction
-    {
-        $this->withRelations = $relations;
+   protected function setUp(): void {
+      parent::setUp();
 
-        return $this;
-    }
+      $this->configureInfolist();
+      $this->configureModal();
+      $this->activitiesUsing = null;
+      $this->modifyTitleUsing = null;
+      $this->shouldModifyTitleUsing = fn() => true;
+      $this->modifyQueryUsing = fn($builder) => $builder;
+      $this->modalHeading = __('activitylog::action.modal.heading');
+      $this->modalDescription = __('activitylog::action.modal.description');
+      $this->query = function(?Model $record) {
+         return Activity::query()
+            ->where(function(Builder $query) use ($record) {
+               $query->where(function(Builder $q) use ($record) {
+                  $q->where('subject_type', $record->getMorphClass())
+                     ->where('subject_id', $record->getKey());
+               })->when($this->getWithRelations(), function(Builder $query, array $relations) use ($record) {
+                  foreach ($relations as $relation) {
+                     $model = get_class($record->{$relation}()->getRelated());
+                     $query->orWhere(function(Builder $q) use ($record, $model, $relation) {
+                        $q->where('subject_type', (new $model)->getMorphClass())
+                           ->whereIn('subject_id', $record->{$relation}()->pluck('id'));
+                     });
+                  }
+               });
+            });
 
-    public function getWithRelations(): ?array
-    {
-        return $this->evaluate($this->withRelations);
-    }
+      };
+   }
 
-    public function timelineIcons(?array $timelineIcons = null): ?StaticAction
-    {
-        $this->timelineIcons = $timelineIcons;
+   private function configureInfolist(): void {
+      $this->infolist(function(?Model $record, Infolist $infolist) {
+         return $infolist
+            ->state(['activities' => $this->getActivityLogRecord($record, $this->getWithRelations())])
+            ->schema($this->getSchema());
+      });
+   }
 
-        return $this;
-    }
+   protected function getActivityLogRecord(?Model $record, ?array $relations = null): Collection {
+      $activities = $this->getActivities($record, $relations);
 
-    public function getTimelineIcons(): ?array
-    {
-        return $this->evaluate($this->timelineIcons);
-    }
+      return $activities->transform(function($activity) {
+         $activity->activityData = $this->formatActivityData($activity);
 
-    public function timelineIconColors(?array $timelineIconColors = null): ?StaticAction
-    {
-        $this->timelineIconColors = $timelineIconColors;
+         return $activity;
+      });
+   }
 
-        return $this;
-    }
+   protected function getActivities(?Model $record, ?array $relations = null): Collection {
+      if ($activities = $this->getActivitiesUsing()) {
+         return $activities;
+      } else {
+         $builder = $this->getQuery()
+            ->latest()
+            ->limit($this->getLimit());
+         $this->getModifyQueryUsing($builder);
 
-    public function getTimelineIconColors(): ?array
-    {
-        return $this->evaluate($this->timelineIconColors);
-    }
+         return $builder
+            ->get();
+      }
+   }
 
-    public function limit(?int $limit = 10): ?StaticAction
-    {
-        $this->limit = $limit;
+   public function getActivitiesUsing(): ?Collection {
+      return $this->evaluate($this->activitiesUsing);
+   }
 
-        return $this;
-    }
+   public function limit(?int $limit = 10): ?StaticAction {
+      $this->limit = $limit;
 
-    public function getLimit(): ?int
-    {
-        return $this->evaluate($this->limit);
-    }
+      return $this;
+   }
 
-    public function query(Closure|Builder|null $query): static
-    {
-        $this->query = $query;
+   public function getQuery(): ?Builder {
+      return $this->evaluate($this->query);
+   }
 
-        return $this;
-    }
+   public function getLimit(): ?int {
+      return $this->evaluate($this->limit);
+   }
 
-    public function getQuery(): ?Builder
-    {
-        return $this->evaluate($this->query);
-    }
+   public function getModifyQueryUsing(Builder $builder): Builder {
+      $this->evaluate($this->modifyQueryUsing, ['builder' => $builder]);
 
-    public function modifyQueryUsing(Closure $closure): static
-    {
-        $this->modifyQueryUsing = $closure;
+      return $builder;
+   }
 
-        return $this;
-    }
+   protected function formatActivityData($activity): array {
+      $value = json_decode($activity->properties, true);
 
-    public function getModifyQueryUsing(Builder $builder): Builder
-    {
-        $this->evaluate($this->modifyQueryUsing, ['builder' => $builder]);
+      return [
+         'log_name' => $activity->log_name,
+         'description' => $activity->description,
+         'subject' => $activity->subject,
+         'event' => $activity->event,
+         'causer' => $activity->causer,
+         'properties' => $this->formatDateValues($value),
+         'batch_uuid' => $activity->batch_uuid,
+         'update' => $activity->updated_at,
+      ];
+   }
 
-        return $builder;
-    }
+   private static function formatDateValues(array|string|null|bool $value): array|string|null|bool {
+      if (is_null($value)) {
+         return $value;
+      }
 
-    public function modifyTitleUsing(Closure $closure): static
-    {
-        $this->modifyTitleUsing = $closure;
+      if (is_array($value)) {
+         foreach ($value as &$item) {
+            $item = self::formatDateValues($item);
+         }
 
-        return $this;
-    }
+         return $value;
+      }
 
-    public function shouldModifyTitleUsing(Closure $closure): static
-    {
-        $this->shouldModifyTitleUsing = $closure;
+      if ($value === false) {
+         return 'false';
+      }
 
-        return $this;
-    }
+      if ($value === true) {
+         return 'true';
+      }
 
-    public function activitiesUsing(Closure $closure): static
-    {
-        $this->activitiesUsing = $closure;
+      if (is_numeric($value)) {
+         return $value;
+      }
 
-        return $this;
-    }
+      try {
 
-    public function getActivitiesUsing(): ?Collection
-    {
-        return $this->evaluate($this->activitiesUsing);
-    }
+         return Carbon::parse($value)
+            ->format(config('filament-activitylog.datetime_format', 'd/m/Y H:i:s'));
+      } catch (InvalidFormatException $e) {
+         return $value;
+      }
+   }
 
-    protected function getActivities(?Model $record, ?array $relations = null): Collection
-    {
-        if ($activities = $this->getActivitiesUsing()) {
-            return $activities;
-        } else {
-            $builder = $this->getQuery()
-                ->latest()
-                ->limit($this->getLimit());
-            $this->getModifyQueryUsing($builder);
+   public function getWithRelations(): ?array {
+      return $this->evaluate($this->withRelations);
+   }
 
-            return $builder
-                ->get();
-        }
-    }
+   private function getSchema(): array {
+      return [
+         TimeLineRepeatableEntry::make('activities')
+            ->schema([
+               TimeLineIconEntry::make('activityData.event')
+                  ->icon(function($state) {
+                     return $this->getTimelineIcons()[$state] ?? 'heroicon-m-check';
+                  })
+                  ->color(function($state) {
+                     return $this->getTimelineIconColors()[$state] ?? 'primary';
+                  }),
+               TimeLineTitleEntry::make('activityData')
+                  ->configureTitleUsing($this->modifyTitleUsing)
+                  ->shouldConfigureTitleUsing($this->shouldModifyTitleUsing),
+               TimeLinePropertiesEntry::make('activityData'),
+               TextEntry::make('log_name')
+                  ->hiddenLabel()
+                  ->badge(),
+               TextEntry::make('updated_at')
+                  ->hiddenLabel()
+                  ->since()
+                  ->badge(),
+            ]),
+      ];
+   }
 
-    protected function getActivityLogRecord(?Model $record, ?array $relations = null): Collection
-    {
-        $activities = $this->getActivities($record, $relations);
+   public function getTimelineIcons(): ?array {
+      return $this->evaluate($this->timelineIcons);
+   }
 
-        return $activities->transform(function ($activity) {
-            $activity->activityData = $this->formatActivityData($activity);
+   public function getTimelineIconColors(): ?array {
+      return $this->evaluate($this->timelineIconColors);
+   }
 
-            return $activity;
-        });
-    }
+   private function configureModal(): void {
+      $this->slideOver()
+         ->modalIcon('heroicon-o-eye')
+         ->modalFooterActions(fn() => [])
+         ->tooltip(__('activitylog::action.modal.tooltip'))
+         ->icon('heroicon-o-bell-alert');
+   }
 
-    protected function formatActivityData($activity): array
-    {
-        return [
-            'log_name'    => $activity->log_name,
-            'description' => $activity->description,
-            'subject'     => $activity->subject,
-            'event'       => $activity->event,
-            'causer'      => $activity->causer,
-            'properties'  => $this->formatDateValues(json_decode($activity->properties, true)),
-            'batch_uuid'  => $activity->batch_uuid,
-            'update'      => $activity->updated_at,
-        ];
-    }
+   public function query(Closure|Builder|null $query): static {
+      $this->query = $query;
 
-    private static function formatDateValues(array|string|null $value): array|string|null
-    {
-        if (is_null($value)) {
-            return $value;
-        }
-
-        if (is_array($value)) {
-            foreach ($value as &$item) {
-                $item = self::formatDateValues($item);
-            }
-
-            return $value;
-        }
-
-        if (is_numeric($value)) {
-            return $value;
-        }
-
-        if (is_bool($value)) {
-            return $value;
-        }
-        
-        try {
-            return Carbon::parse($value)
-                ->format(config('filament-activitylog.datetime_format', 'd/m/Y H:i:s'));
-        } catch (InvalidFormatException $e) {
-            return $value;
-        }
-    }
+      return $this;
+   }
 
 }
